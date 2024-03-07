a88c413a19c848938d9b74c5c7f864dbe7dae6d1
Thanhungcuong <78573263+Thanhungcuong@users.noreply.github.com>
2024-03-07 15:10:01 +0700

    first

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],content: ["index.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "bgbg": "linear-gradient(90deg, #50C577 0%, #2DBEFC 100%)",
+      },
+    },
+  },
+  plugins: [],
+}
+
