f9ece5e5393ca415cc14102973c9079ee9bbe3b1
Thanhungcuong <78573263+Thanhungcuong@users.noreply.github.com>
2024-03-19 11:45:31 +0700

    19/3/24

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         "bgbg": "linear-gradient(90deg, #50C577 0%, #2DBEFC 100%)",
+        "orangee": "linear-gradient(180deg, #FFB469 0%, #E57709 100%)",
       },
       colors: {
         'bode': '#EBEBEB',
